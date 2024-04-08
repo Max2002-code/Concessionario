bb916dabcf05d363110b29a220f214d31ef5ce43
Massimo Berlingeri <mfede071@gmail.com>
2024-04-08 09:35:24 +0200

    film

--- Netflix/src/app/app.module.ts
+++ Netflix/src/app/app.module.ts
@@ -1,35 +1,109 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'; // Importa il modulo HttpClient
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Route } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { ProfileComponent } from './components/profile/profile.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
-import { AppRoutingModule } from './app-routing.module';
-import { LocandinaComponent } from './components/locandina/locandina.component';
-import { LocandinaService } from './locandina.service';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DetailsComponent } from './components/details/details.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
+import { ListaUtentiComponent } from './components/lista-utenti/lista-utenti.component';
+import { AuthGuard } from './auth/auth.guard';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TokenInterceptor } from './auth/token.interceptor';
+
+const rotte: Route[] = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
+        children: [
+            {
+                path: 'login',
+                component: LoginComponent
+            }
+        ]
+    },
+    {
+        path: 'movies',
+        component: MoviesComponent,
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'movies/:id',
+        component: MovieDetailsComponent,
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'utenti',
+        component: ListaUtentiComponent,
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'profile',
+        component: ProfileComponent,
+        canActivate: [AuthGuard],
+        children: [
+            {
+                path: 'details',
+                component: DetailsComponent
+            },
+            {
+                path: 'favorites',
+                component: FavoritesComponent
+            }
+        ]
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+]
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    NavbarComponent,
-    HomeComponent,
-    LocandinaComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    AppRoutingModule,
-    FormsModule
-  ],
-  providers: [
-    LocandinaService
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MoviesComponent,
+        ProfileComponent,
+        NavbarComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent,
+        DetailsComponent,
+        FavoritesComponent,
+        ListaUtentiComponent,
+        MovieDetailsComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        RouterModule.forRoot(rotte),
+        NgbModule
+    ],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: TokenInterceptor,
+            multi: true
+        }
+    ],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- Netflix/src/app/components/navbar/navbar.component.ts
+++ Netflix/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
-import { AuthServiceService } from 'src/app/auth-service.service';
+import { Component, OnInit } from '@angular/core';
+import { Auth } from 'src/app/auth/auth.interface';
+import { AuthService } from 'src/app/auth/auth.service';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
-  constructor(private authServiceService: AuthServiceService) { }
+export class NavbarComponent implements OnInit {
 
-  isLoggedIn(): boolean {
-    return this.authServiceService.isLoggedIn();
-  }
+    isMenuCollapsed = true;
+    utente!: Auth | null;
+
+    constructor(private authSrv: AuthService) {}
+
+    ngOnInit(): void {
+        this.authSrv.user$.subscribe((_utente) => {
+            this.utente = _utente;
+        });
+    }
+
+    logout() {
+        this.authSrv.logout();
+    }
 }
